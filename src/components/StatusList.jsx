8894f2a1a1284918aef0194407c6dacc1b2333de
Federico Mozzon <mozzonfederico@gmail.com>
2020-02-21 13:37:25 -0300

    Internacionalizo el status list

--- src/components/StatusList.jsx
+++ src/components/StatusList.jsx
@@ -11,13 +11,14 @@ import {
   OUTDATED,
   FAILURE,
 } from '../constants/status'
+import intl from 'react-intl-universal'
 
 //Wrapper para renderizar una lista en funcion el estado de la request a la API
 
 const StatusList = ({ status, items, render_item }) => 
-  status === PENDING ? <LoadingMessage title='Un segundo' description='Estamos obteniendo el listado del recurso' />
+  status === PENDING ? <LoadingMessage title={intl.get('API_ERROR_MESSAGE_TITLE')} description={intl.get('API_ERROR_MESSAGE')} />
     : status === SUCCESS || status === OUTDATED ? <List items={items} render_item={render_item} />
-      : status === FAILURE ? <ErrorMessage title='Lo sentimos' errors={['Hubo un error tratando de obtener el listado del recurso']} />
+      : status === FAILURE ? <ErrorMessage title={intl.get('API_LOAD_MESSAGE_TITLE')} errors={[intl.get('API_LOAD_MESSAGE')]} />
         : null
 
 export default StatusList
